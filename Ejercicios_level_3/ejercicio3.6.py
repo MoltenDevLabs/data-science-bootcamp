2624bd865483fe084a9370182b674dc695e60711
Pol Cadavid <polkadavid@gmail.com>
2024-06-12 14:07:49 +0200

    Completado ejercicio3.6 que contiene una funcion que recibe una nota y devuelve la calificacion cualitativa

--- Ejercicios_level_3/ejercicio3.6.py
+++ Ejercicios_level_3/ejercicio3.6.py
@@ -1,2 +1,20 @@
 # Escribir una función que reciba una lista de notas y devuelva la lista de calificaciones correspondientes a esas notas.
 
+def calificacion(n):
+  c = ''
+  if n<5:
+    c = 'Suspenso'
+  elif n<7:
+    c = 'Aprobado'
+  elif n<9:
+    c = 'Notable'
+  elif n<10:
+    c = 'Sobresaliente'
+  else:
+    c = 'Matricula'
+  
+  return c
+
+notas = [6.67, 2.86, 8.64, 7.01, 4.9]
+nota_cualitativa = list(map(calificacion, notas))
+print(nota_cualitativa)
--- map.py
+++ map.py
@@ -1,4 +1,5 @@
 # Función map
+# Para mas ejemplo mirar ejercicio3.6
 
 n = [1, 2, 3, 4]
 r = map(lambda x: x+x, n)
@@ -8,4 +9,4 @@ print(list(r))
 m, M = [1, 2, 3, 4], [5, 6, 7, 8]
 rr = map(lambda x,y: x+y, m, M)
 
-print(list(r))
+print(list(rr))
